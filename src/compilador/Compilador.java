bb9d196fb4371e07917702454d8f678ad9afa60a
xdbrian <xdbrian.steventh@gmail.com>
2015-04-11 19:00:04 -0430

    Cambios initial body

--- src/compilador/Compilador.java
+++ src/compilador/Compilador.java
@@ -6,6 +6,7 @@ import java_cup.runtime.*;
 public class Compilador {
 	
 	/***********
+         * 
 	SymbolFactory es una nueva caracteristica que ha sido a�adida a las version 11a de cup, la cual facilita la implementacion de clases Symbol personalizadas
 	, esto debido a que dicha clase no provee mucha informaci�n de contexto que podria ser util para el analisis semantico o ayudar en la construccion del AST
 	Mas informacion en: http//4thmouse.com/index.php/2007/02/15/using-custom-symbols-in-cup/
